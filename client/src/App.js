67c75cfde98a7e93c3d7b958fc80ff1a35adc828
Alex Miller <alexmillerprojects@gmail.com>
2020-02-13 17:26:18 -0800

    added api call and set to state

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,32 @@
 import React from 'react';
+import axios from 'axios';
 
 import './App.css';
 
-function App() {
-  
-    return (
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      players: []
+    }
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:5000/api/footballers').then(res => {
+      console.log('footballers', res.data);
+      this.setState({ players: res.data })
+    }).catch(error => {
+      console.log('this is an error', error)
+    })
+  }
+
+    render() {
+      return (
     <div className="App">
       <h1>Happy building <span role="img" aria-label="Smiling emoji">😃</span></h1>
     </div>
   );
+    }
 }
 
 export default App;
